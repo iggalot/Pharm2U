7d422c212bf31c3acf46a70b3fabc5d527c1351a
James <Allen>
2020-01-21 15:04:15 -0600

    Installed EntityFramework.  Created the DBContext models from the database schema.  Began refactoring work on IDataService to accomodate both MockData and EntityFrameworkData services.

---
+++ Pharm2U/Services/Data/EntityFramework/EntityFrameworkDataService.cs
@@ -0,0 +1,13 @@
+﻿using Pharm2U.Models;
+
+namespace Pharm2U.Services.Data.EntityFramework
+{
+    /// <summary>
+    /// Controls interaction with the EntityFramework package and its
+    /// communication with the SQLSERVER 2019.
+    /// </summary>
+    public class EntityFrameworkDataService : IDataService<OrderModel>
+    {
+
+    }
+}
